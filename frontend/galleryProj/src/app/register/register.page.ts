69bff80145f89ca4162323f31244903866668b5d
Ryno Botes <rynob1998@gmail.com>
2021-11-16 22:40:13 +0200

    Register page in working order

--- frontend/galleryProj/src/app/register/register.page.ts
+++ frontend/galleryProj/src/app/register/register.page.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { HomePage } from '../home/home.page';
+import { BASE_API_URL } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+
+
+let members = [];
+
+
 
 @Component({
   selector: 'app-register',
@@ -10,7 +17,7 @@ import { HomePage } from '../home/home.page';
 export class RegisterPage {
   
 
-  constructor(private modalController:ModalController){}
+  constructor(private modalController:ModalController, public http:HttpClient){}
   
 
 
@@ -25,5 +32,41 @@ export class RegisterPage {
   });
   return await modal.present();
 }
+addmember= ()=>{
+  // ev.preventDefault();
+let member ={
+  memberId:  (<HTMLInputElement>document.getElementById('memberId')).value,
+ memberFName: (<HTMLInputElement>document.getElementById('memberFName')).value,
+ memberLName: (<HTMLInputElement>document.getElementById('memberLName')).value,
+ memberEmail: (<HTMLInputElement>document.getElementById('memberEmail')).value
+}
+  
+
+  fetch(BASE_API_URL+"member-info",{method:'POST',body: JSON.stringify(member),headers: {
+		'Content-type': 'application/json; charset=UTF-8'
+	}
+}).then(function (response) {
+	if (response.ok) {
+		return response.json();
+	}
+	return Promise.reject(response);
+}).then(function (data) {
+	console.log(data);
+}).catch(function (error) {
+	console.warn('Something went wrong.', error);
+});;
+  // members.push(member);
+  document.forms[0].reset();
+
+  console.warn(JSON.stringify(member));
+  
 
 }
+
+}
+
+
+
+// document.addEventListener('DOMContentLoader', () =>{
+//   document.getElementById('registerbtn').addEventListener('click',addmember);
+// });
